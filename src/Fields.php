520c63936bc89290b93ba45dbe121431f06d9759
Roman Marintsenko <inoryy@gmail.com>
2012-01-10 23:08:52 +0200

    added constants

---
+++ src/Fields.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace iPizza;
+
+/**
+ * List of all fields used by iPizza protocol
+ *
+ * @author Roman Marintsenko <inoryy@gmail.com>
+ * @since  10.01.2011
+ */
+final class Fields
+{
+    const SERVICE_ID        = 'VK_SERVICE';
+    const VERSION           = 'VK_VERSION';
+    const RECIEVER_ID       = 'VK_SND_ID';
+    const ORDER_ID          = 'VK_STAMP';
+    const SUM               = 'VK_AMOUNT';
+    const CURRENCY          = 'VK_CURR';
+    const RECIEVER_BANK_ACC = 'VK_ACC';
+    const RECIEVER_NAME     = 'VK_NAME';
+    const ORDER_REFERENCE   = 'VK_REF';
+    const DESCRIPTION       = 'VK_MSG';
+    const CHARSET           = 'VK_CHARSET';
+    const CHECKSUM          = 'VK_MAC';
+    const SUCCESS_URL       = 'VK_RETURN';
+    const CANCEL_URL        = 'VK_CANCEL';
+    const USER_LANG         = 'VK_LANG';
+
+    /**
+     * Can't instantiate this class
+     */
+    private function __construct()
+    {
+
+    }
+}
---
+++ src/Services.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace iPizza;
+
+/**
+ * List of all services available via iPizza
+ *
+ * @author Roman Marintsenko <roman.marintsenko@knplabs.com>
+ * @since  10.01.2012
+ */
+final class Services
+{
+    // Requests
+    const PAYMENT_REQUEST      = '1001';
+    const AUTHENTICATE         = '3001';
+
+    // Reponses
+    const PAYMENT_SUCCESS      = '1101';
+    const PAYMENT_CANCEL       = '1901';
+    const PAYMENT_ERROR        = '1902';
+
+    /**
+     * Can't instantiate this class
+     */
+    private function __construct()
+    {
+
+    }
+
+}
