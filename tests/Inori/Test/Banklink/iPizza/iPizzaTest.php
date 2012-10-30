7cf6dedab1cd895d9288cc29f567be19dcd795b5
Roman Marintsenko <inoryy@gmail.com>
2012-10-30 23:37:13 +0200

    attempt to get stuff working with another test

--- tests/Inori/Test/Banklink/iPizza/iPizzaTest.php
+++ tests/Inori/Test/Banklink/iPizza/iPizzaTest.php
@@ -17,9 +17,9 @@ class iPizzaTest extends \PHPUnit_Framework_TestCase
     public function setUp()
     {
         $this->iPizza = new iPizza(
-            'uid200486',
+            'uid258629',
             'Test Testov',
-            '1234567890',
+            '119933113300',
             __DIR__.'/Fixture/private_key.pem',
             __DIR__.'/Fixture/public_key.pem',
             'http://www.google.com',
@@ -32,11 +32,11 @@ class iPizzaTest extends \PHPUnit_Framework_TestCase
         $expected = array(
           'VK_SERVICE' => '1001',
           'VK_VERSION' => '008',
-          'VK_SND_ID'  => 'uid200486',
+          'VK_SND_ID'  => 'uid258629',
           'VK_STAMP'   => '1',
           'VK_AMOUNT'  => '100',
           'VK_CURR'    => 'EUR',
-          'VK_ACC'     => '1234567890',
+          'VK_ACC'     => '119933113300',
           'VK_NAME'    => 'Test Testov',
           'VK_REF'     => '13',
           'VK_MSG'     => 'Test payment',
@@ -45,7 +45,7 @@ class iPizzaTest extends \PHPUnit_Framework_TestCase
           'VK_RETURN'  => 'http://www.google.com',
           'VK_CANCEL'  => 'http://www.google.com',
           'VK_LANG'    => 'ENG',
-          'VK_MAC'     => 'fltl/Nnqbi3YGZwe1bY9hC23Kj7fImqm0CqQbpRKleJXfW+/tF+s5qRl+Tlz0FIpTSYFVHBMwLOXNVzG0JUVHJRyZeTkoAovot7ffLL1plycfPizgcATdZBzN5qsM+qFIbXXzvmoNGGBDGo0jFU7LCJBdq/JLM5JWNcVLVO6r67skkH/6k2cFoaAF0b00nfDFaKytJVivRcBqie1H0G+fAXNLJCCm+lfrr0bSG1duGwDuzpXONpyl2zUxjDJXXQurIEM8ffilvJDZC81VIe1Q/VHNVflumquXn5ETjrHQPmqSvFloUMh2gwi8wtuOyfhMfsS99B+B8yx4rfuUR+Kiw=='
+          'VK_MAC'     => 'g4SMbCZEbxSXF7qx8ggcRHTyWOx4Dqkb0eM6atoEC5A12SAlWDgIw5TnB319KtreUcEubrjZz9z4NQgVrSieoOX9yr3G7ciLopGaoajAr6RA9RTYP0QDoArTuDKBqFwRT6D+erTggu9Dz3G/dQKlL9SCQtUxV6yCHp0cLgzYmtUGXoC7x4WnP1NuJZwlBnJI3acsCNyw5gTnEHle0Xd2OElH84aKlItqSsPbFirWhZRLfLy8uyiwSseChnTnDXCINyFLypHNTvvn+DaE8m+nyDkL4Jt3L2rciYkLPuoXSY3JGXTzjS7TkpOPUEtBQZ65ZylltduAeknxocvSZYUskA=='
         );
 
         $request = $this->iPizza->preparePaymentRequest('1', 'Test payment', '100', 'ENG', 'EUR');
@@ -58,13 +58,13 @@ class iPizzaTest extends \PHPUnit_Framework_TestCase
         $response = array(
             'VK_SERVICE'  => '1101',
             'VK_VERSION'  => '008',
-            'VK_SND_ID'   => 'HP',
-            'VK_REC_ID'   => 'uid200486',
+            'VK_SND_ID'   => 'GENIPIZZA',
+            'VK_REC_ID'   => 'uid258629',
             'VK_STAMP'    => '1',
             'VK_T_NO'     => '17944',
             'VK_AMOUNT'   => '100',
             'VK_CURR'     => 'EUR',
-            'VK_REC_ACC'  => '1234567890',
+            'VK_REC_ACC'  => '119933113300',
             'VK_REC_NAME' => 'Test Testov',
             'VK_REF'      => '13',
             'VK_MSG'      => 'Test payment',
@@ -73,7 +73,7 @@ class iPizzaTest extends \PHPUnit_Framework_TestCase
             'VK_AUTO'     => 'N',
             'VK_SND_NAME' => 'Test Account Owner',
             'VK_SND_ACC'  => '221234576897',
-            'VK_MAC'      => '0Rcj4zimJpLmnqoFPeAChALa/QuA6OD5J/GJAudNU8iDxGylN7FHoC0RPeOuAo8E6P5wAM4zOaFwW7Fd4q0APSb8xBJbmztf1uxQXPWuD9aR2BGO06KL+HLrZ8yM+vh4941NA3ybg3EIoZzcd5Qqfk0BMopLy6RHC70rnwI1wceOn9k7+ar79MNFQU8Q38KeOdczqRZx/svlTLaUvPmBQNvJj9GXRVKTWFEBmnzJ+NEM0OAGoCbUaeZDoSJN1OvetloHpYt2McchT5XPPKinO/VpKr/WMi7UorfT+cbmjuCxgji/R0UJO1ZCWzUKO2D572ULZqG7J7FIvXzI9R/Ydw=='
+            'VK_MAC'      => 'eOhoii13T3KdtNl5YrTvV/RPIaVuml1eCjzrkL3NwhQ45qcrdwKmiRYzv47phfauU6BrTJo7nz2LygrjNMNLjfJQZ62GrGRzn5KqhitJ3YAAdZ8lQr2tY9voV1qa8ZevIPqu9yQg2W0zDVI4o4itB7X/BkbhheMbIhYBPuikAQDRUZqIYUVUil6DrB98Tk8xc3rJoouJkuS8OqOuGRUTNywG49834CmV2PZEi7NHj9lXmXtuDlDWVUdHBXKx6nVsgTLIWRDMFTvDfL2A/OXiNNklkYzEj4DWpaCd3CJyfIjD0xtdA4g+BRRObhqiYN0sBddBa10NMp5wUoflGwfcpw=='
         );
 
         $this->assertTrue($this->iPizza->verifyPaymentResponse($response));
