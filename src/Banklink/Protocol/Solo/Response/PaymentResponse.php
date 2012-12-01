f82c05906ea655b3b4b0151fff3c267e47502cae
Roman Marintsenko <inoryy@gmail.com>
2012-12-01 14:00:05 +0200

    Added a warning about Solo payment response containing much less data than other protocols

--- src/Banklink/Protocol/Solo/Response/PaymentResponse.php
+++ src/Banklink/Protocol/Solo/Response/PaymentResponse.php
@@ -5,7 +5,9 @@ namespace Banklink\Protocol\Solo\Response;
 use Banklink\Response\PaymentResponse as BasePaymentResponse;
 
 /**
- * Payment response representation for Solo Protocol
+ * Payment response representation for Solo protocol
+ *
+ * NB! Solo payment response has much less data than other protocols, you must manually set it to use in places like view
  *
  * @author Roman Marintsenko <inoryy@gmail.com>
  * @since  01.12.2012
