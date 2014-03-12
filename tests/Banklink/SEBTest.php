eaf15e85c9a646d08e370a6b630715bdde0e3690
Артём Курапов <artkurapov@gmail.com>
2014-03-12 11:17:09 +0200

    Update SEBTest.php

--- tests/Banklink/SEBTest.php
+++ tests/Banklink/SEBTest.php
@@ -52,4 +52,42 @@ class SEBTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($expectedRequestData, $request->getRequestData());
         $this->assertEquals('https://www.seb.ee/cgi-bin/unet3.sh/un3min.r', $request->getRequestUrl());
     }
+    
+    public function testPreparePaymentRequestWithStringKeys()
+    {
+
+        $protocol = new iPizza(
+            'uid258629',
+            'Test Testov',
+            '119933113300',
+            file_get_contents(__DIR__.'/data/iPizza/private_key.pem'),
+            file_get_contents(__DIR__.'/data/iPizza/public_key.pem'),
+            'http://www.google.com'
+        );
+
+        $this->seb = new SEB($protocol);
+
+        $expectedRequestData = array(
+          'VK_SERVICE'  => '1001',
+          'VK_VERSION'  => '008',
+          'VK_SND_ID'   => 'uid258629',
+          'VK_STAMP'    => '1',
+          'VK_AMOUNT'   => '100',
+          'VK_CURR'     => 'EUR',
+          'VK_ACC'      => '119933113300',
+          'VK_NAME'     => 'Test Testov',
+          'VK_REF'      => '13',
+          'VK_MSG'      => 'Test payment',
+          'VK_RETURN'   => 'http://www.google.com',
+          'VK_CANCEL'   => 'http://www.google.com',
+          'VK_LANG'     => 'ENG',
+          'VK_CHARSET'  => 'UTF-8',
+          'VK_MAC'      => 'g4SMbCZEbxSXF7qx8ggcRHTyWOx4Dqkb0eM6atoEC5A12SAlWDgIw5TnB319KtreUcEubrjZz9z4NQgVrSieoOX9yr3G7ciLopGaoajAr6RA9RTYP0QDoArTuDKBqFwRT6D+erTggu9Dz3G/dQKlL9SCQtUxV6yCHp0cLgzYmtUGXoC7x4WnP1NuJZwlBnJI3acsCNyw5gTnEHle0Xd2OElH84aKlItqSsPbFirWhZRLfLy8uyiwSseChnTnDXCINyFLypHNTvvn+DaE8m+nyDkL4Jt3L2rciYkLPuoXSY3JGXTzjS7TkpOPUEtBQZ65ZylltduAeknxocvSZYUskA=='
+        );
+
+        $request = $this->seb->preparePaymentRequest(1, 100, 'Test payment', 'ENG', 'EUR');
+
+        $this->assertEquals($expectedRequestData, $request->getRequestData());
+        $this->assertEquals('https://www.seb.ee/cgi-bin/unet3.sh/un3min.r', $request->getRequestUrl());
+    }
 }
